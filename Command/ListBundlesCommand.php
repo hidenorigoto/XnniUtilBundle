5d1c7c598ff58e93b2a25fdd3ec55b2386ac4d3c
hidenorigoto <hidenorigoto@gmail.com>
2011-03-16 13:47:38 +0900

    added container:list-bundles command

---
+++ Command/ListBundlesCommand.php
@@ -0,0 +1,36 @@
+<?php
+namespace Xnni\UtilBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+use Symfony\Bundle\FrameworkBundle\Command\Command;
+
+class ListBundlesCommand extends Command
+{
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('container:list-bundles')
+        ;
+    }
+
+    /**
+     * @see Command
+     *
+     * @throws \InvalidArgumentException When runs in non-dev environment
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $kernel = $this->container->get('kernel');
+        foreach ($kernel->getBundles() as $bundle) {
+            $output->writeln($bundle->getName());
+        }
+    }
+}
+
--- README.rst
+++ README.rst
@@ -2,15 +2,19 @@
 Xnni/UtilBundle
 ===============
 
-Utilities.
+Utilities for Symfony2 development.
 
 
 --------
 Commands
 --------
 
-* util:container-param (parametername)
+* ``$ php app/console container:check-param (parametername)``
 
    Dumps container parameters with specified parameter name.
+
+* ``$ php app/console container:list-bundles``
+
+   Displays a list of bundles currentry activated in the kernel.
 
 

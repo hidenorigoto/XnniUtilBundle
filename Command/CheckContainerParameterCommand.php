a35fd0fe9316a5418c657641a433e1891cedb614
hidenorigoto <hidenorigoto@gmail.com>
2011-06-25 10:24:30 +0900

    applied RC1 change (Command to ContainerAwareCommand)

--- Command/CheckContainerParameterCommand.php
+++ Command/CheckContainerParameterCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
-use Symfony\Bundle\FrameworkBundle\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand as Command;
 
 class tempContainer extends \appDevDebugProjectContainer
 {
